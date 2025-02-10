683ec64a416062961b55e0fc5cce8ccb00de7f67
YogitaPatil5 <yogitarajput.04@gmail.com>
2025-02-10 15:33:05 +0530

    code update

--- analysis.py
+++ analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 
 def sum_value():
-    df = pd.read_csv("data/SalaryData.csv")
+    df = pd.read_csv(r"data/SalaryData.csv")
     return df["Salary"].sum()
 if __name__ == "__main__":
     print(f"Sum of salary: {sum_value()}")
